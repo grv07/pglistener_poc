8c5724db06c69a01443e65f3fa7482d3b03e1be7
Gaurav Tyagi <grvtyagi22@gmail.com>
2022-11-26 12:40:28 +0530

    add sql for adding trigger and notify func on every table update

---
+++ src/listener_setup.sql
@@ -0,0 +1,32 @@
+-- Add a table update notification function
+CREATE OR REPLACE FUNCTION table_update_notify() RETURNS trigger AS $$
+DECLARE
+  id int;
+  key varchar;
+  value varchar;
+BEGIN
+  IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+    id = NEW.id;
+    key = NEW.key;
+    value = NEW.val;
+  ELSE
+    id = OLD.id;
+    key = OLD.key;
+    value = OLD.val;
+  END IF;
+  PERFORM pg_notify('table_update', json_build_object('table', TG_TABLE_NAME, 'id', id, 'key', key, 'value', value, 'action_type', TG_OP)::text);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Add UPDATE table trigger
+DROP TRIGGER users_notify_update ON my_table;
+CREATE TRIGGER users_notify_update AFTER UPDATE ON my_table FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+
+-- Add INSERT table trigger
+DROP TRIGGER users_notify_insert ON my_table;
+CREATE TRIGGER users_notify_insert AFTER INSERT ON my_table FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+
+-- Add DELETE table trigger
+DROP TRIGGER users_notify_delete ON my_table;
+CREATE TRIGGER users_notify_delete AFTER DELETE ON my_table FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
